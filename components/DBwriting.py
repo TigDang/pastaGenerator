1142de89f5938dec2a1fa802668d3a7fae7674b6
TigDang <gizetdinov.m.f@gmail.com>
2022-06-14 20:35:23 +0300

    add pull recipies function at the DBwriting component

--- components/DBwriting.py
+++ components/DBwriting.py
@@ -11,5 +11,20 @@ def pullIngridients(connect, distinctIngridients):
             cursor.execute(sql)
 
 def pullRecipies(connect, titles, ingredients):
-    print('something')
-    # TODO: make this
+    pastaID = 0
+    ingrID = 0
+    pastaIDquery = "SELECT pasta_id FROM pasta WHERE pasta_name = '{}'"
+    ingrIDquery = "SELECT ingr_id FROM ingredient WHERE ingr_name = '{}'"
+    with connect.cursor() as cursor:
+        for i in range(len(titles)):
+            for ingr in ingredients[i]:
+                cursor.execute(pastaIDquery.format(titles[i]))
+                pastaID = cursor.fetchall()[0][0]
+                cursor.execute(ingrIDquery.format(ingr))
+                ingrID = cursor.fetchall()[0][0]
+
+                print(titles[i], ingr, pastaID, ingrID, sep='~')
+
+                sql = "INSERT INTO recipe (pasta_assoc_id, ingrd_assoc_id) VALUES ('{}', '{}')".format(pastaID,
+                                                                                                       ingrID)
+                cursor.execute(sql)
